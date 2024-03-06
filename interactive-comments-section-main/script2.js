fb8f958a89ddbdaf8042dfe6f8b27a5cf0b6c746
BadrSI <mouad.simo2001@gmail.com>
2024-03-06 23:06:56 +0100

    fix: fixing some styles and add the div wapper for commnets.

--- interactive-comments-section-main/script2.js
+++
@@ -1,7 +0,0 @@
-import data from './data.json' assert {type: 'json'}
-console.log('s2',data)
-
-console.log('s2', 'comments', data.comments);
-data.comments.push({commetn: 'thest'});
-console.log('s2', 'comments', data.comments);
-// branch 2
